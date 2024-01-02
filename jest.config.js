ef4384e8a53e77777fd532409dbcc66d59076776
Kwasi <stawuah18@gmail.com>
2024-01-02 00:35:40 +0000

    :package:

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,6 @@ module.exports = {
     // setupFilesAfterEnv: ['<rootDir>/test-setup.js'], // Setup file to run before each test suite
     coverageDirectory: './src/main.js', // Directory for coverage reports
     collectCoverageFrom: ['src/**/*.js'], // Files to include in code coverage analysis
-    moduleNameMapper: {
-      // Mock CSS and other file imports in Jest
-      '\\.(css|scss)$': 'identity-obj-proxy',
-      // Handle image imports in Jest
-      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': '<rootDir>/fileMock.js',
-    },
     collectCoverage: true,
     verbose: true,
     coverageDirectory: "coverage",
