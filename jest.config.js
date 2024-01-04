385d603df839ff6be2d6aaf4a3b044f76e14a4db
Kwasi <stawuah18@gmail.com>
2024-01-04 17:45:48 +0000

    jest.config :package:

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     testEnvironment: 'node', // or 'jsdom' for browser-like environment
     testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
-    // setupFilesAfterEnv: ['<rootDir>/test-setup.js'], // Setup file to run before each test suite
     coverageDirectory: './src/main.js', // Directory for coverage reports
     collectCoverageFrom: ['src/**/*.js'], // Files to include in code coverage analysis
     collectCoverage: true,
